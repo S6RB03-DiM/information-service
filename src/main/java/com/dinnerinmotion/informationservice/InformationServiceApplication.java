bcd87f7636b931702d4078f0fcc858e6696a2b09
Dimitar Petrov <d.petrov-ab@student.fontys.nl>
2022-06-11 16:49:23 +0200

    test with internal db

--- src/main/java/com/dinnerinmotion/informationservice/InformationServiceApplication.java
+++ src/main/java/com/dinnerinmotion/informationservice/InformationServiceApplication.java
@@ -2,6 +2,7 @@ package com.dinnerinmotion.informationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class InformationServiceApplication {
